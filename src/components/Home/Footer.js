ca7cefa148b852c3a44a75b319c1d7cd076d3963
rana <trana51997@gmail.com>
2022-11-27 01:31:13 -0600

    DONE

--- src/components/Home/Footer.js
+++ src/components/Home/Footer.js
@@ -5,75 +5,81 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <footer class="footer">
-      <div class="container grid--footer">
-        <div class="logo-col">
-          <Link href="index.html" class="footer-logo">
-            <img src={logo} class="logo" alt="Logo of Marketplace" />
+    <footer className="footer">
+      <div className="container grid--footer">
+        <div className="logo-col">
+          <Link href="index.html" className="footer-logo">
+            <img src={logo} className="logo" alt="Logo of Marketplace" />
           </Link>
-          <p class="copyright">
+          <p className="copyright">
             Copyright &copy; 2022 by Marketplace, Inc. All rights reserved.
           </p>
         </div>
 
-        <div class="social-link-col">
-          <p class="footer-heading">Social media</p>
+        <div className="social-link-col">
+          <p className="footer-heading">Social media</p>
 
-          <ul class="social-link">
+          <ul className="social-link">
             <li>
-              <Link to="#" class="footer-link">
-                <ion-icon name="logo-instagram" class="icon-link"></ion-icon>
+              <Link to="#" className="footer-link">
+                <ion-icon
+                  name="logo-instagram"
+                  className="icon-link"
+                ></ion-icon>
               </Link>
             </li>
 
             <li>
-              <Link to="#" class="footer-link">
-                <ion-icon name="logo-facebook" class="icon-link"></ion-icon>
+              <Link to="#" className="footer-link">
+                <ion-icon name="logo-facebook" className="icon-link"></ion-icon>
               </Link>
             </li>
 
             <li>
-              <Link to="#" class="footer-link">
-                <ion-icon name="logo-twitter" class="icon-link"></ion-icon>
+              <Link to="#" className="footer-link">
+                <ion-icon name="logo-twitter" className="icon-link"></ion-icon>
               </Link>
             </li>
           </ul>
         </div>
 
-        <div class="resources-col">
-          <p class="footer-heading">Resources</p>
-          <ul class="resources-link">
+        <div className="resources-col">
+          <p className="footer-heading">Resources</p>
+          <ul className="resources-link">
             <li>
-              <Link to="#" class="footer-link">
+              <Link to="#" className="footer-link">
                 Feedback
               </Link>
             </li>
             <li>
-              <Link to="#" class="footer-link">
+              <Link to="#" className="footer-link">
                 Blog
               </Link>
             </li>
             <li>
-              <Link to=" #" class="footer-link">
+              <Link to=" #" className="footer-link">
                 Developers
               </Link>
             </li>
             <li>
-              <Link to="#" class="footer-link">
+              <Link to="#" className="footer-link">
                 Terms & conditions
               </Link>
             </li>
           </ul>
         </div>
 
-        <div class="contact-col">
-          <p class="footer-heading">Contact</p>
-          <address class="contact">
-            <p class="address">701 S Nedderman Dr, Arlington, TX 76019</p>
-            <Link class="footer-link" to="tel:817-272-2011">
+        <div className="contact-col">
+          <p className="footer-heading">Contact</p>
+          <address className="contact">
+            <p className="address">701 S Nedderman Dr, Arlington, TX 76019</p>
+            <Link className="footer-link" to="tel:817-272-2011">
               817-272-2011
             </Link>
-            <Link class="footer-link" to="mailto:marketplace@marketplace.com">
+            <Link
+              className="footer-link"
+              to="mailto:marketplace@marketplace.com"
+            >
               marketplace@marketplace.com
             </Link>
           </address>
